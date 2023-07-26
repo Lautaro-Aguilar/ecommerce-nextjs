54b80e07a5f87230e289abf9c008999ea4755efd
Lautaro Aguilar <aguilarlautaroexequiel867@gmail.com>
2023-07-25 22:09:09 -0300

    feat(backend): :sparkles: Create client, get products and banner data

---
+++ ecommerce/sanityUtils.js
@@ -0,0 +1,29 @@
+// sanity.js
+import {createClient} from '@sanity/client'
+import imageUrlBuilder from '@sanity/image-url'
+// Import using ESM URL imports in environments that supports it:
+// import {createClient} from 'https://esm.sh/@sanity/client'
+
+export const client = createClient({
+  projectId: '2q5exs4w',
+  dataset: 'production',
+  useCdn: true, // set to `false` to bypass the edge cache
+  apiVersion: '2023-05-03', // use current date (YYYY-MM-DD) to target the latest API version
+  token: process.env.SANITY_SECRET_TOKEN, // Only if you want to update content with the client
+})
+
+const builder = imageUrlBuilder(client)
+
+export const urlFor = (source) => builder.image(source)
+
+export async function getProducts() {
+  const query = '*[_type == "product"]'
+  const products = await client.fetch(query)
+  return products
+}
+
+export async function getBanner() {
+  const query = '*[_type == "banner"]'
+  const banner = await client.fetch(query)
+  return banner
+}
