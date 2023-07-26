41495db0dc4f029dcba8e0c646c8d207554d0986
Lautaro Aguilar <aguilarlautaroexequiel867@gmail.com>
2023-07-25 21:32:14 -0300

    feat(UI): :sparkles: Create base components

--- src/app/page.js
+++ src/app/page.js
@@ -2,17 +2,20 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 import React from "react";
+import { HeroBanner, Footer, Product } from "./Components";
 
 const Home = () => {
   return (
     <>
-      Hero Banner
+      <HeroBanner />
       <div className='products-heading'>
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
-      <div>{["Product 1", "Product 2"].map((product) => product)}</div>
-      Footer
+      <div className='products-container'>
+        {["Product 1", "Product 2"].map((product) => product)}
+      </div>
+      <Footer />
     </>
   );
 };
