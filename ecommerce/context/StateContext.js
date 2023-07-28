45b4920fe4a8bb3ee4f14c608a259ee925dc6c9d
Lautaro Aguilar <aguilarlautaroexequiel867@gmail.com>
2023-07-28 15:11:16 -0300

    feat(UI): :sparkles: create function for remove item from cart, and inc or dec item in cart

--- ecommerce/context/StateContext.js
+++ ecommerce/context/StateContext.js
@@ -5,11 +5,14 @@ import {toast} from 'react-hot-toast'
 const Context = createContext()
 
 export const StateContext = ({children}) => {
-  const [showCart, setSetShowCart] = useState(false)
+  const [showCart, setShowCart] = useState(false)
   const [cartItems, setCartItems] = useState([])
   const [totalPrice, setTotalPrice] = useState(0)
   const [totalQuantities, setTotalQuantities] = useState(0)
   const [qty, setQty] = useState(1)
+
+  let foundProduct
+  let index
 
   const onAdd = (product, quantity) => {
     const checkProductInCart = cartItems.find((item) => item._id === product._id)
@@ -42,9 +45,48 @@ export const StateContext = ({children}) => {
       return prevQty - 1
     })
 
+  const onRemove = (product) => {
+    foundProduct = cartItems.find((item) => item._id === product._id)
+    const newCartItems = cartItems.filter((item) => item._id !== product._id)
+
+    setTotalPrice((prevTotalPrice) => prevTotalPrice - foundProduct.price * foundProduct.quantity)
+    setTotalQuantities((prevTotalQuantities) => prevTotalQuantities - foundProduct.quantity)
+    setCartItems(newCartItems)
+  }
+
+  const toggleCartItemQuanitity = (id, value) => {
+    foundProduct = cartItems.find((item) => item._id === id)
+    index = cartItems.findIndex((product) => product._id === id)
+    const newCartItems = cartItems.filter((item) => item._id !== id)
+
+    if (value === 'inc') {
+      setCartItems([...newCartItems, {...foundProduct, quantity: foundProduct.quantity + 1}])
+      setTotalPrice((prevTotalPrice) => prevTotalPrice + foundProduct.price)
+      setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + 1)
+    } else if (value === 'dec') {
+      if (foundProduct.quantity > 1) {
+        setCartItems([...newCartItems, {...foundProduct, quantity: foundProduct.quantity - 1}])
+        setTotalPrice((prevTotalPrice) => prevTotalPrice - foundProduct.price)
+        setTotalQuantities((prevTotalQuantities) => prevTotalQuantities - 1)
+      }
+    }
+  }
+
   return (
     <Context.Provider
-      value={{showCart, cartItems, totalPrice, totalQuantities, qty, incQty, decQty, onAdd}}
+      value={{
+        showCart,
+        cartItems,
+        totalPrice,
+        totalQuantities,
+        qty,
+        incQty,
+        decQty,
+        onAdd,
+        setShowCart,
+        toggleCartItemQuanitity,
+        onRemove,
+      }}
     >
       {children}
     </Context.Provider>
