13a9dd642a94e0255eb938fba82c955469f9ffdc
Lautaro Aguilar <aguilarlautaroexequiel867@gmail.com>
2023-07-28 00:01:56 -0300

    feat(UI): :sparkles: Create page for product detail

---
+++ src/app/product/[slug]/page.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { getProduct } from "../../../../ecommerce/sanityUtils";
+import { Product } from "@/app/Components";
+import {
+  AiFillStar,
+  AiOutlineMinus,
+  AiOutlinePlus,
+  AiOutlineStar,
+} from "react-icons/ai";
+import ImageContainer from "./ImageContainer";
+import ProductDetailDesc from "./ProductDetailDesc";
+
+async function Slug({ params }) {
+  const { product, products } = await getProduct({ slug: params.slug });
+
+  const { image, name, details, price } = product;
+
+  return (
+    <div>
+      <div className='product-detail-container'>
+        <div>
+          <ImageContainer image={image} />
+        </div>
+
+        <ProductDetailDesc
+          product={product}
+          name={name}
+          price={price}
+          details={details}
+        />
+      </div>
+
+      <div className='maylike-products-wrapper'>
+        <h2>You may also like</h2>
+        <div className='marquee'>
+          <div className='maylike-products-container track'>
+            {products.map((item) => (
+              <Product key={item._id} product={item} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Slug;
