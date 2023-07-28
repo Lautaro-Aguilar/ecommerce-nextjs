e671039bed5684c3c462ec8f78f985a3294a5889
Lautaro Aguilar <aguilarlautaroexequiel867@gmail.com>
2023-07-28 15:11:51 -0300

    fix(UI): :bug: it is solved that it sent to product/product

--- src/app/Components/Product.jsx
+++ src/app/Components/Product.jsx
@@ -5,7 +5,7 @@ import { getProducts, urlFor } from "../../../ecommerce/sanityUtils";
 const Product = async ({ product: { image, name, slug, price } }) => {
   return (
     <div>
-      <Link href={`product/${slug.current}`}>
+      <Link href={`/product/${slug.current}`}>
         <div className='product-card'>
           <img
             src={urlFor(image && image[0]).url()}
